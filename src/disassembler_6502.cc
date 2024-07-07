55736b2c2bd0ebeace616fa350d4ba94ebeb2412
massimilianofronza <maxfronza97@gmail.com>
2024-07-07 23:59:28 +0200

    First hex opcodes implemented in disassembler

--- src/disassembler_6502.cc
+++ src/disassembler_6502.cc
@@ -1,25 +1,54 @@
 #include <fstream>
+#include <iomanip> // for std::setw
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+    // The NES is an 8-bit console. 8 bits -> 2 hex digits
+    // 2 hex digits make 1 byte
+    // The first couple of bytes is 454e
+    // Little-endian machines like the NES store data with the most significant byte as second
+    // This means that 4e is going to be in position 0, and 45 in position 1 of the pc
 int disassemble6502op(const unsigned char *buffer, const unsigned int pc) {
     const unsigned char *code = &buffer[pc];
     int opbytes = 1;
 
-    // 2 hex digits make 1 byte (8 bits)
-    // The first couple of bytes is 454e
-    // Little-endian machines like the NES store data with the most significant byte as second
-    // This means that 4e is going to be in position 0, and 45 in position 1 of the pc
+    // The 6502 has 151 valid opcodes out of a possible 256(8 bits/2 hex per code)
+    // The rest are undocumented opcodes
     switch (*code) {
         case 0x00:
-            cout << pc << ": NOP\n"; break;
-        case 0x01:
-            cout << pc << ": boh\n"; break;
+            cout << pc << ": BRK" << endl; break;
+        case 0x08:
+            cout << pc << ": PHP" << endl; break;
+        case 0x12:
+            cout << pc << ": JAM" << endl; break;
+        case 0x1a:
+            cout << pc << ": NOP (undocumented)" << endl; break;
+        case 0x4e:
+            cout << pc << ": LSR oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+            opbytes = 3; break;
+        case 0x6e:
+            cout << pc << ": ROR oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+            opbytes = 3; break;
+        case 0x75:
+            cout << pc << ": ADC oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            opbytes = 2; break;
+        case 0x85:
+            cout << pc << ": STA oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << ")" << endl;
+            opbytes = 2; break;
+        case 0xad:
+            cout << pc << ": LDA oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+            opbytes = 3; break;
+        case 0xff:
+            cout << pc << ": ISC oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),"
+                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
         default:
-            cout << pc << ": /: " << *code << "\n"; break;
-        //case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
+            cout << pc << ": todo: " << setw(2) << setfill('0') << static_cast<int>(*code) << endl; return 131088; break;
     }
 
     return opbytes;
@@ -43,7 +72,7 @@ int main (int argc, char **argv) {
     file.seekg(0, ios::end);            // move the get pointer at the end of the file
     streampos fsize = file.tellg();     // get its position(size fo the file)
     file.seekg(0, ios::beg);            // move it back at the beginning
-    cout << "fsize of given input: " << fsize << " hexadecimal numbers, half-bytes, " << fsize << " " << endl;
+    cout << "fsize of given input: " << fsize << " bytes" << endl;
     
     // create a buffer to read the file
     vector<unsigned char> buffer(fsize);
@@ -60,10 +89,20 @@ int main (int argc, char **argv) {
     
     unsigned char *this_buffer = buffer.data();
     unsigned int pc = 0;
-return 0;
+
+    // switch to hexadecimals for all the following outputs
+    cout << hex;
+
     while (pc < fsize) {
         pc += disassemble6502op(this_buffer, pc);
+        /*if (pc>10) {
+            return 0;
+        }*/
     }
+
+    // switch to decimals for all the following outputs
+    cout << dec;
+
     /*
     for (const auto &elem : buffer) {
         cout << elem << " ";
