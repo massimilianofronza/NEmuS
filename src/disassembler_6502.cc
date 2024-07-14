8e03d936351dd30016585a446b5aee1431e01baf
massimilianofronza <maxfronza97@gmail.com>
2024-07-14 23:55:34 +0200

    Update on opcodes implementation

--- src/disassembler_6502.cc
+++ src/disassembler_6502.cc
@@ -19,33 +19,274 @@ int disassemble6502op(const unsigned char *buffer, const unsigned int pc) {
     switch (*code) {
         case 0x00:
             cout << pc << ": BRK" << endl; break;
+        case 0x01:
+            cout << pc << ": ORA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            opbytes = 2; break;
+        case 0x06:
+            cout << pc << ": ASL " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
         case 0x08:
             cout << pc << ": PHP" << endl; break;
+        case 0x0e:
+            cout << pc << ": ASL " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x10:
+            cout << pc << ": BPL " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x11:
+            cout << pc << ": ORA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),Y" << endl;
+            opbytes = 2; break;
         case 0x12:
             cout << pc << ": JAM" << endl; break;
+        case 0x16:
+            cout << pc << ": ASL " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0x17:
+            cout << pc << ": SLO " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0x18:
+            cout << pc << ": CLC" << endl; break;
+        case 0x19:
+            cout << pc << ": ORA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
         case 0x1a:
             cout << pc << ": NOP (undocumented)" << endl; break;
+        case 0x1b:
+            cout << pc << ": SLO " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
+        case 0x1c:
+            cout << pc << ": NOP (undocumented)" << endl;
+            opbytes = 3; break;
+        case 0x1d:
+            cout << pc << ": ORA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x1e:
+            cout << pc << ": ASL " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x20:
+            cout << pc << ": JSR " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x22:
+            cout << pc << ": JAM" << endl; break;
+        case 0x23:
+            cout << pc << ": RLA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            opbytes = 2; break;
+        case 0x24:
+            cout << pc << ": BIT " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x27:
+            cout << pc << ": RLA " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x28:
+            cout << pc << ": PLP" << endl; break;
+        case 0x2a:
+            cout << pc << ": ROL A" << endl; break;
+        case 0x2c:
+            cout << pc << ": BIT " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x2d:
+            cout << pc << ": AND " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x2e:
+            cout << pc << ": ROL " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x2f:
+            cout << pc << ": RLA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x32:
+            cout << pc << ": JAM" << endl; break;
+        case 0x34:
+            cout << pc << ": NOP (undocumented)" << endl;
+            opbytes = 2; break;
+        case 0x36:
+            cout << pc << ": ROL " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0x38:
+            cout << pc << ": SEC" << endl; break;
+        case 0x39:
+            cout << pc << ": AND " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
+        case 0x3b:
+            cout << pc << ": RLA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
+        case 0x3c:
+            cout << pc << ": NOP (undocumented)" << endl;
+            opbytes = 3; break;
+        case 0x3e:
+            cout << pc << ": ROL " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x3f:
+            cout << pc << ": RLA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x40:
+            cout << pc << ": RTI" << endl; break;
+        case 0x44:
+            cout << pc << ": NOP (undocumented)" << endl;
+            opbytes = 2; break;
+        case 0x45:
+            cout << pc << ": EOR " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x46:
+            cout << pc << ": LSR " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x4c:
+            cout << pc << ": JMP " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
         case 0x4e:
-            cout << pc << ": LSR oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
-                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+            cout << pc << ": LSR " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x50:
+            cout << pc << ": BVC " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x53:
+            cout << pc << ": SRE (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),Y" << endl;
+            opbytes = 2; break;
+        case 0x61:
+            cout << pc << ": ADC (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            opbytes = 2; break;
+        case 0x6b:
+            cout << pc << ": ARR #" << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x6c:
+            cout << pc << ": JMP (" << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                    << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
             opbytes = 3; break;
         case 0x6e:
-            cout << pc << ": ROR oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
-                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+            cout << pc << ": ROR " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
             opbytes = 3; break;
         case 0x75:
-            cout << pc << ": ADC oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            cout << pc << ": ADC " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0x78:
+            cout << pc << ": SEI" << endl; break;
+        case 0x7b:
+            cout << pc << ": RRA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
+        case 0x7c:
+            cout << pc << ": NOP " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x7d:
+            cout << pc << ": ADC " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x80:
+            cout << pc << ": NOP " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x81:
+            cout << pc << ": STA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),X" << endl;
+            opbytes = 2; break;
+        case 0x82:
+            cout << pc << ": NOP (undocumented)" << endl;
+            opbytes = 2; break;
+        case 0x83:
+            cout << pc << ": SAX (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X)" << endl;
+            opbytes = 2; break;
+        case 0x84:
+            cout << pc << ": STY " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
             opbytes = 2; break;
         case 0x85:
-            cout << pc << ": STA oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << ")" << endl;
+            cout << pc << ": STA " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
             opbytes = 2; break;
-        case 0xad:
-            cout << pc << ": LDA oper(" << setw(2) << setfill('0') << static_cast<int>(code[1])
-                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << ")" << endl;
+        case 0x87:
+            cout << pc << ": SAX " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x8d:
+            cout << pc << ": STA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
             opbytes = 3; break;
+        case 0x8e:
+            cout << pc << ": STX " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x8f:
+            cout << pc << ": SAX " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0x90:
+            cout << pc << ": BCC " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0x91:
+            cout << pc << ": STA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),Y" << endl;
+            opbytes = 2; break;
+        case 0x92:
+            cout << pc << ": JAM" << endl; break;
+        case 0x93:
+            cout << pc << ": (unstable) SHA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),Y" << endl;
+            opbytes = 2; break;
+        case 0x95:
+            cout << pc << ": STA " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0x9d:
+            cout << pc << ": STA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0x9f:
+            cout << pc << ": SHA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",Y" << endl;
+            opbytes = 3; break;
+        case 0xa7:
+            cout << pc << ": LAX " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0xa8:
+            cout << pc << ": TAY" << endl; break;
+        case 0xad:
+            cout << pc << ": LDA " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0xb1:
+            cout << pc << ": LDA (" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),Y" << endl;
+            opbytes = 2; break;
+        case 0xb4:
+            cout << pc << ": LDY " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0xc5:
+            cout << pc << ": CMP " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0xc8:
+            cout << pc << ": INY" << endl; break;
+        case 0xd0:
+            cout << pc << ": BNE " << setw(2) << setfill('0') << static_cast<int>(code[1]) << endl;
+            opbytes = 2; break;
+        case 0xd2:
+            cout << pc << ": JAM" << endl; break;
+        case 0xd5:
+            cout << pc << ": CMP " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
+        case 0xd8:
+            cout << pc << ": CLD" << endl; break;
+        case 0xde:
+            cout << pc << ": DEC " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << ",X" << endl;
+            opbytes = 3; break;
+        case 0xed:
+            cout << pc << ": SBC " << setw(2) << setfill('0') << static_cast<int>(code[1])
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            opbytes = 3; break;
+        case 0xf5:
+            cout << pc << ": SBC " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ",X" << endl;
+            opbytes = 2; break;
         case 0xff:
-            cout << pc << ": ISC oper(" << setw(2) << setfill('0') << static_cast<int>(code[1]) << "),"
-                                        << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
+            cout << pc << ": ISC " << setw(2) << setfill('0') << static_cast<int>(code[1]) << ","
+                                   << setw(2) << setfill('0') << static_cast<int>(code[2]) << endl;
             opbytes = 3; break;
         default:
             cout << pc << ": todo: " << setw(2) << setfill('0') << static_cast<int>(*code) << endl; return 131088; break;
