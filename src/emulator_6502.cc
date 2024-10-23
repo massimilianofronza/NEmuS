625975a51a7dac4dd67368a04c1e703196915caa
massimilianofronza <maxfronza97@gmail.com>
2024-10-23 18:14:22 +0200

    No new and delete: smart pointers and arrays

--- src/emulator_6502.cc
+++ src/emulator_6502.cc
@@ -1,36 +1,47 @@
-#include <iomanip> // for std::setw
+#include <array>        // for array
+#include <iomanip>      // for setw
 #include <iostream>
-#include <cstdint>
-#include <sstream> // for ostringstream
+#include <cstdint>      // for uint8_t, uint16_t
+#include <memory>       // for smart pointers (unique_ptr, make_unique)
+#include <sstream>      // for ostringstream
+#include <stdexcept>    // for runtime_error
 
 using namespace std;
 
-// Flags for the Processor Status
-#define flag_C 0    // Carry
-#define flag_Z 1    // Zero
-#define flag_I 2    // Interrupt Disable
-#define flag_D 3    // Decimal Mode
-#define flag_B 4    // Break Command
-//      flag_???    // bit 5 is unused
-#define flag_V 6    // Overflow
-#define flag_N 7    // Negative
+
+// Flags for the Processor Status - constexpr is generally better than #define flag_C 0, which just places a 0 where you write a flag_C
+constexpr uint8_t flag_C = 0;    // Carry
+constexpr uint8_t flag_Z = 1;    // Zero
+constexpr uint8_t flag_I = 2;    // Interrupt Disable
+constexpr uint8_t flag_D = 3;    // Decimal Mode
+constexpr uint8_t flag_B = 4;    // Break Command
+//                flag_???       // bit 5 is unused
+constexpr uint8_t flag_V = 6;    // Overflow
+constexpr uint8_t flag_N = 7;    // Negative
+
+
+// Physical hardware dimensions
+constexpr uint32_t RAM_SIZE = 65536;    // 64KB for the 6502
+
 
 struct State6502 {
-    uint8_t A;          // Accumulator register
-    uint8_t X;          // Index Register X
-    uint8_t Y;          // Index Register Y
-    uint8_t *RAM;       // Random Access Memory
-    uint8_t SP;         // Stack Pointer, memory locations $0100-$01FF
-    uint16_t PC;        // Program Counter
-    bool *P;            // Processor Status
+    uint8_t A;                      // Accumulator register
+    uint8_t X;                      // Index Register X
+    uint8_t Y;                      // Index Register Y
+    array<uint8_t, RAM_SIZE> RAM;   // Random Access Memory // TODO test speed between this, a smart pointer, a vector and a basic RAM[size] object
+    uint8_t SP;                     // Stack Pointer, memory locations $0100-$01FF
+    uint16_t PC;                    // Program Counter
+    array<bool, 8> P;               // Processor Status
 };
 
-void unimplemented(State6502 *state) {
+void unimplemented(unique_ptr<State6502>& state) {
     cerr << "Unimplemented instruction!";
     exit(1);
 }
 
-void emulate6502op(State6502 *state) {
+// reference for the smart pointer because just passing it by value would result in an error: you can't transfer the ownership of a unique_ptr
+// because they are meant to have a unique ownership
+void emulate6502op(unique_ptr<State6502>& state) {
     const unsigned char *code = &state->RAM[state->PC];
     int opbytes = 1;
 
@@ -44,7 +55,7 @@ void emulate6502op(State6502 *state) {
         case 0xff: unimplemented(state); break;
         default:
             ostringstream errorMsg;
-            errorMsg << "emulate_6502op() -> Unexpected opcode: 0x" << setw(2) << setfill('0') << hex << static_cast<int>(code[0]) << dec << endl;
+            errorMsg << "emulate_6502op() -> Unexpected opcode: 0x" << setw(2) << setfill('0') << hex << static_cast<int>(code[0]) << dec;
             throw runtime_error(errorMsg.str());
     }
 
@@ -54,12 +65,9 @@ void emulate6502op(State6502 *state) {
 int main(int argc, char **argv) {
 
     // initialization
-    State6502 *state = new State6502();
-    state->RAM = new uint8_t[10];
-    state->P = new bool[7];
-    for (uint8_t i=0; i<7; i++) {
-        state->P[i] = 0;
-    }
+    unique_ptr<State6502> state = make_unique<State6502>();
+    state->RAM.fill(0);
+    state->P.fill(0);
 
     // initialize a demo CPU State
     state->RAM[0] = 0x00;
@@ -67,14 +75,15 @@ int main(int argc, char **argv) {
     state->RAM[2] = 0x01;
     state->PC = 2;
 
-    emulate6502op(state);
+    try {
+        emulate6502op(state);
+    }
+    catch (const runtime_error& e) {
+        cerr << e.what() << endl;
+    }
 
     state->A = 42;
     cout << static_cast<int>(state->A) << endl;
-
-    delete[] state->P;
-    delete[] state->RAM;
-    delete state;
 
     return 0;
 }
