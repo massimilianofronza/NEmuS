5d417e7ed68edd7e0ec1814fb52fe3d0c8bfb791
massimilianofronza <maxfronza97@gmail.com>
2024-10-29 20:21:11 +0100

    Refactoring State6502 class

--- include/State6502.h
+++ include/State6502.h
@@ -1,10 +1,17 @@
 #include <array>        // for array
+#include <iomanip>      // for setw
+#include <iostream>
+#include <memory>       // for smart pointers (unique_ptr, make_unique)
+// #include <sstream>      // for ostringstream
+// #include <stdexcept>    // for runtime_error
+
 #include "config.h"
 
 using namespace std;
 
 // struct members are public by default, unlike classes where they are private
 class State6502 {
+    
     public:
         uint8_t A;                      // Accumulator register
         uint8_t X;                      // Index Register X
@@ -13,4 +20,40 @@ class State6502 {
         uint8_t SP;                     // Stack Pointer, memory locations $0100-$01FF, 256-511
         uint16_t PC;                    // Program Counter
         array<bool, 8> P;               // Processor Status
+
+        // initialization - TODO test with init and non-init RAM, it shouldn't matter
+        State6502() {
+            RAM.fill(0);
+            P.fill(0);
+        }
+
+        // reference for the smart pointer because just passing it by value would result in an error: you can't transfer the ownership of a unique_ptr
+        // because they are meant to have a unique ownership
+        void emulate6502op(unique_ptr<State6502>& state) {
+            const unsigned char *code = &state->RAM[state->PC];
+            int opbytes = 1;
+
+            switch (*code) {
+                case 0x00:
+                    cout << "0x00\n";
+                    state->P[flag_B]=1;
+                    opbytes=2;
+                    break;    // TODO should I put the break flag of the Processor Status to 1?
+                /*....*/
+                case 0xff: unimplemented(state); break;
+                default:
+                    ostringstream errorMsg;
+                    errorMsg << "emulate_6502op() -> Unexpected opcode: 0x" << setw(2) << setfill('0') << hex << static_cast<int>(code[0]) << dec;
+                    throw runtime_error(errorMsg.str());
+            }
+
+            state->PC += opbytes;
+        }
+    
+    private:
+        
+        void unimplemented(unique_ptr<State6502>& state) {
+            cerr << "Unimplemented instruction!";
+            exit(1);
+        }
 };
--- src/main.cc
+++ src/main.cc
@@ -1,57 +1,20 @@
-#include <iomanip>      // for setw
-#include <iostream>
-#include <cstdint>      // for uint8_t, uint16_t
-#include <memory>       // for smart pointers (unique_ptr, make_unique)
-#include <sstream>      // for ostringstream
-#include <stdexcept>    // for runtime_error
 #include "State6502.h"
 
 using namespace std;
 
 
-void unimplemented(unique_ptr<State6502>& state) {
-    cerr << "Unimplemented instruction!";
-    exit(1);
-}
-
-// reference for the smart pointer because just passing it by value would result in an error: you can't transfer the ownership of a unique_ptr
-// because they are meant to have a unique ownership
-void emulate6502op(unique_ptr<State6502>& state) {
-    const unsigned char *code = &state->RAM[state->PC];
-    int opbytes = 1;
-
-    switch (*code) {
-        case 0x00:
-            cout << "0x00\n";
-            state->P[flag_B]=1;
-            opbytes=2;
-            break;    // TODO should I put the break flag of the Processor Status to 1?
-        /*....*/
-        case 0xff: unimplemented(state); break;
-        default:
-            ostringstream errorMsg;
-            errorMsg << "emulate_6502op() -> Unexpected opcode: 0x" << setw(2) << setfill('0') << hex << static_cast<int>(code[0]) << dec;
-            throw runtime_error(errorMsg.str());
-    }
-
-    state->PC += opbytes;
-}
-
 int main(int argc, char **argv) {
 
-    // initialization - TODO test with init and non-init RAM, it shouldn't matter
     unique_ptr<State6502> state = make_unique<State6502>();
-    state->RAM.fill(0);
-    state->P.fill(0);
-
+    
     // initialize a demo CPU State
     state->RAM[0] = 0x00;
     state->RAM[1] = 0xff;
     state->RAM[2] = 0x01;
-    state->PC = 2;
+    state->PC = 0;
 
     try {
-        emulate6502op(state);
+        state->emulate6502op(state);
     }
     catch (const runtime_error& e) {
         cerr << e.what() << endl;
